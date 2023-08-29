ceac77cbe5d0bff024c09aad24cd9ef5e133a863
Hussein Hussein <husseinwkc@gmail.com>
2023-08-29 19:43:46 -0400

    vercel

--- qurantester/src/App.vue
+++ qurantester/src/App.vue
@@ -2,13 +2,14 @@
   <div >
     <page-header/>
     <router-view/>
+    <Analytics/>
   </div>
 </template>
 <script>
 import PageHeader from './components/PageHeader.vue';
-
+import {Analytics} from '@vercel/analytics'
 export default {
-  components: { PageHeader },
+  components: { PageHeader , Analytics},
    data() {
      return {
         choice: {}
