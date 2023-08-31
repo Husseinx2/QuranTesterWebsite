5561cbd7460d95538d6df894aa22f4974a29d73a
Hussein Hussein <husseinwkc@gmail.com>
2023-08-31 01:47:48 -0400

    fixed spacing for header and spacing for middle section

--- qurantester/src/components/AboutSection.vue
+++ qurantester/src/components/AboutSection.vue
@@ -25,42 +25,48 @@
       <ul>
         <li>
           <p>
-            The Prophet (ﷺ) said: "It shall be said - meaning to the one who
-            memorized the Qur'an - 'Recite, and rise up, recite (melodiously) as
-            you would recite in the world. For indeed your rank shall be at the
-            last Ayah you recited.'"
+            <i>
+              The Prophet (ﷺ) said: "It shall be said - meaning to the one who
+              memorized the Qur'an - 'Recite, and rise up, recite (melodiously)
+              as you would recite in the world. For indeed your rank shall be at
+              the last Ayah you recited.'"
+            </i>
+            <br />
+            <b> <span>(Jami` at-Tirmidhi 2914)</span></b>
           </p>
-          <b> <span>(Jami` at-Tirmidhi 2914)</span></b>
         </li>
         <li>
           <p>
-            The Prophet (ﷺ) said: "If anyone recites the Qur'an and acts
-            according to its content, on the Day of Judgement his parents will
-            be given to wear a crown whose light is better than the light of the
-            sun in the dwellings of this world if it were among you. So what do
-            you think of him who acts according to this ?"
+            <i>
+              The Prophet (ﷺ) said: "If anyone recites the Qur'an and acts
+              according to its content, on the Day of Judgement his parents will
+              be given to wear a crown whose light is better than the light of
+              the sun in the dwellings of this world if it were among you. So
+              what do you think of him who acts according to this ?"
+            </i>
+            <br />
+            <b> <span>(Sunan Abi Dawud 1453)</span></b>
           </p>
-          <b> <span>(Sunan Abi Dawud 1453)</span></b>
         </li>
       </ul>
     </section>
     <h1 class="Reminder">Message From Us</h1>
     <section class="support">
       <ul>
-      <li>
-        <p>
-           We ask that you be patient with us as this website is still not
-          fully complete. If there are any problems with the website or if you
-          have any feedback you can contact us through
-          <a href="https://www.linkedin.com/in/hussein210/">LinkedIn</a>.
-        </p>
-      </li>
-      <li>
-        <p>
-           We also ask that you to keep all the people who worked on this
-          website in your du'a
-        </p>
-      </li>
+        <li>
+          <p>
+            We ask that you be patient with us as this website is still not
+            fully complete. If there are any problems with the website or if you
+            have any feedback you can contact us through
+            <a href="https://www.linkedin.com/in/hussein210/">LinkedIn</a>.
+          </p>
+        </li>
+        <li>
+          <p>
+            We also ask that you to keep all the people who worked on this
+            website in your du'a
+          </p>
+        </li>
       </ul>
     </section>
   </div>
@@ -124,7 +130,7 @@ h5 {
   position: relative;
   margin-left: 15px;
 }
-.supportHeader {
+.reminder {
   grid-area: supportHeader;
 }
 
